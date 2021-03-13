423908065194d83f0bec657662a92ac6049bc6d6
didue <jordan.hans7@gmail.com>
2021-03-14 01:29:02 +0900

    add google adsense

--- _config.js
+++ _config.js
@@ -32,6 +32,6 @@ module.exports = {
   googleAnalytics: '',     // Google Analytics TrackingID. ex.'UA-123456789-0'
   googleSearchConsole: '', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
   googleAdsenseSlot: '',   // Google Adsense Slot. ex.'5214956675'
-  googleAdsenseClient: '', // Google Adsense Client. ex.'ca-pub-5001380215831339'
+  googleAdsenseClient: 'ca-pub-1986476617358545', // Google Adsense Client. ex.'ca-pub-5001380215831339'
     // Please correct the adsense client number(ex.5001380215831339) in the './static/ads.txt' file.
 };
